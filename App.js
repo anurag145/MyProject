879003254b60ed36f3b5c3ae72d2c0a1ca85a206
Anurag Shrivastava <anuragshrivastava@Anurags-Macbook-Air.local>
2018-10-23 13:47:23 +0530

    code-push support

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import CodePush from 'react-native-code-push';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -16,8 +16,15 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component {
+
+  componentDidMount(){
+    CodePush.sync({
+      updateDialog:true,
+      installMode:CodePush.InstallMode.IMMEDIATE
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
